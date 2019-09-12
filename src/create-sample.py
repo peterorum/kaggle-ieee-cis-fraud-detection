db58bed9e9d21b05639eb855e77082d0faff06ff
Peter Orum <peter@peterorum.com>
2019-09-12 17:03:47 +1000

    test sample

--- src/create-sample.py
+++ src/create-sample.py
@@ -2,10 +2,13 @@
 
 import pandas as pd
 
-train = pd.read_csv("../input/ieee-fraud-detection/train_transaction.csv.zip")
-
 sample_size = 10000
 
-sample = train.sample(sample_size)
 
+train = pd.read_csv("../input/ieee-fraud-detection/train_transaction.csv.zip")
+sample = train.sample(sample_size)
 sample.to_csv('../input/ieee-fraud-detection/train_transaction-sample.csv', index=False)
+
+test = pd.read_csv("../input/ieee-fraud-detection/test_transaction.csv.zip")
+sample = test.sample(sample_size)
+sample.to_csv('../input/ieee-fraud-detection/test_transaction-sample.csv', index=False)
